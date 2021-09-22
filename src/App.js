1262c30f2af89eba0f1b6173fa462cd441b43d87
m3xpl4y <m3xpl4y@gmail.com>
2021-09-22 13:17:26 +0200

    set weather container to middle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import MainWeatherCard from './components/MainWeatherCard';
 import SearchInput from './components/SearchInput';
 
 function App() {
   return (
       <div className="App">
           <SearchInput/>
+          <MainWeatherCard/>
       </div>
   );
 }

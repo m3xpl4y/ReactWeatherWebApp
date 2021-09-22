b6dc8d1c4617be20066d6faa2e3472a43f369ff7
m3xpl4y <m3xpl4y@gmail.com>
2021-09-22 13:56:43 +0200

    added input state location & fixed card to center

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 
 export default class SearchInput extends Component{
+    constructor() {
+        super()
+        this.state = {
+            location: ''
+        };
+    }
+  
+    handleChange(e) {
+        this.setState({
+            location: e.target.value
+        });
+    }
+   
     render(){
         return(
         <div className="m-input">
-            <input type="text" className="m-input"
-            placeholder="Search Location"></input>
+            <input onChange={e => this.setState({ location: e.target.value })} 
+            value={this.state.location}
+            type="text" className="m-input"
+            placeholder="Search Location"
+            ></input>
         </div>
+
         )    
         }
     }
